d667faee76a8fe39bcd15a0932667009890e31ed
ShkarupaDC <dimon.shkarupa@gmail.com>
2021-12-24 23:56:32 +0200

    Fix filtering by non-existent attribute

--- main/src/lib/filtering/apply-filters.ts
+++ main/src/lib/filtering/apply-filters.ts
@@ -4,8 +4,10 @@ import { FilterMap } from '@lib/filtering/interfaces/filter-map';
 export const applyFilters = (items: Item[], filters: FilterMap): Item[] => {
   const fields = Object.keys(filters) as (keyof Item)[];
   const filteredItems = items.filter((item) => {
-    return fields.every((field) =>
-      filters[field]?.isSatisfiedBy(item[field] as string | number)
+    return fields.every(
+      (field) =>
+        field in item &&
+        filters[field]?.isSatisfiedBy(item[field] as string | number)
     );
   });
   return filteredItems;
